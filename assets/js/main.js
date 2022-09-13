28885aca1355c405ed07c4fb556fa06b9d56a9b2
Andrea289 <andrea.mazzeo95@gmail.com>
2022-09-13 22:52:51 +0200

    fix backend

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 const bash = document.querySelector("#bash");
 const bashContent = document.querySelector("#bash-content");
 
-import jsonData from './../json/projects.json' assert {type: 'json'};
+import jsonData from './../json/data.json' assert {type: 'json'};
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
@@ -41,7 +41,6 @@ async function startBash()
 async function handleNewCommand()
 {
   const [valid, readCommand, command] = getInputCommand();
-  console.log("Valid: " + valid + " Command: " + command);
 
   removeOldHeadingLine();
   
@@ -69,14 +68,11 @@ function getInputCommand()
 
 function validateCommand(command) 
 {
-  return command === "help" ||
-         command === "projects" ||
-         command === "social" ||
-         command === "about me" ||
-         command === "resume" ||
-         command === "cls" ||
-         command === "adaptive background subtraction" ||
-         command === "research";
+  const projects = Object.keys(jsonData.projects);
+  const papers = Object.keys(jsonData.papers);
+  let availableCommands = [...jsonData.commands, ...projects, ...papers];
+
+  return availableCommands.includes(command);
 }
 
 function handleCommand(command)
@@ -84,43 +80,21 @@ function handleCommand(command)
   if (command === "help")
   {
     createText("Available commands listed below. Type <span class='lightblue-text'>\"cls\"</span> to clear terminal.");
-    printCommandsItem("projects");
-    printCommandsItem("research");
-    printCommandsItem("social");
-    printCommandsItem("about me");
-    printCommandsItem("resume");
+    printCommands();
   }
   else if (command === "projects")
   {
     createText("Type the project name to see details.");
-    printProjectsItem("Instagram bot");
-    printProjectsItem("GAN");
-    printProjectsItem("Paper");
-    printProjectsItem("FPGA Fault Injector");
-    printProjectsItem("Set Associative Cache");
-    printProjectsItem("Adaptive Background subtraction");
-    printProjectsItem("3D object reconstruction");
-    printProjectsItem("Euridit italia");
-    printProjectsItem("Lorenzo il magnifico");
-    printProjectsItem("Object Tracker");
-    printProjectsItem("Nessie 2013");
+    printProjects();
   }
-  else if (command = "research")
+  else if (command === "publications")
   {
-    printProjectsItem("Usability-based Cross-Layer Reliability Evaluation of Image Processing Applications");
-    printProjectsItem("Approximation-Based Fault Tolerance in Image Processing Applications");
-    printProjectsItem("Error Modeling for Image Processing Filters accelerated onto SRAM-based FPGAs");
-  }
-  else if (command === "adaptive background subtraction") {
-
-    const data = getProjectInfo(command);
-    console.log(data);
-    showProjectInfo(data);
+    createText("Type the name of the publication to see details.");
+    printResearch();
   }
   else if (command === "about me")
   {
     createText("In ipsum dolore do voluptate duis magna excepteur occaecat. Est adipisicing aliqua proident eu ut ex incididunt. Cillum enim consectetur sit deserunt Lorem reprehenderit tempor eiusmod laborum nostrud deserunt pariatur irure. Aute sunt reprehenderit occaecat aute incididunt. Minim consequat dolore veniam magna ad in magna est dolor ad do.");
-    console.log(projectsData);
   }
   else if (command === "social")
   {
@@ -137,6 +111,14 @@ function handleCommand(command)
   {
     bashContent.querySelectorAll("div").forEach(e => e.parentNode.removeChild(e));
     bashContent.querySelectorAll("p").forEach(e => e.parentNode.removeChild(e));
+  }
+  else if (isProject(command))
+  {
+    showProjectInfo(jsonData.projects[command]);
+  }
+  else if (isPublication(command))
+  {
+    showPublicationInfo(jsonData.papers[command]);
   }
 }
 
@@ -220,16 +202,6 @@ function createText(text)
   bashContent.appendChild(p);
 }
 
-function printCommandsItem(text) 
-{
-  printListItem(text, "lightblue-text");
-}
-
-function printProjectsItem(text) 
-{
-  printListItem(text, "lightgreen-text");
-}
-
 function printListItem(text, colorClass)
 { 
   const div = document.createElement("div");
@@ -266,21 +238,6 @@ function downloadFile(url, fileName) {
     });
 };
 
-function getProjectInfo(project)
-{
-  console.log("Searching: " + project);
-
-  for (let el in jsonData.projects) {
-    let obj  = jsonData.projects[el]
-    console.log(obj);
-    if (obj.key == project) {
-      console.log("found: " + project);
-      return obj;
-    }
-  }
-  return undefined;
-}
-
 function showProjectInfo(data)
 {
   const div = document.createElement("div");
@@ -290,7 +247,7 @@ function showProjectInfo(data)
   h1.textContent = data.name;
 
   const img = document.createElement("img");
-  const imgSource = "./assets/img/projects/" + data.media[0] + ".png";
+  const imgSource = "./assets/img/projects/" + data.media + ".png";
   img.setAttribute("src", imgSource);
 
   const overviewH2 = document.createElement("h2");
@@ -323,6 +280,61 @@ function showProjectInfo(data)
   div.appendChild(divIcons);
 
   bashContent.appendChild(div);
+}
+
+function showPublicationInfo(data)
+{
+  const div = document.createElement("div");
+  div.setAttribute("class", "project-container");
+
+  const h1 = document.createElement("h1");
+  h1.textContent = data.name;
+
+  const published = document.createElement("p");
+  published.innerText = data.published;
+  
+  const abstract = document.createElement("p");
+  abstract.innerText = data.abstract;
+
+  div.appendChild(h1);
+  div.appendChild(published);
+  div.appendChild(abstract);
+  bashContent.appendChild(div);
+}
+
+function printCommands()
+{
+  jsonData.commands.forEach(element => {
+    printListItem(element, "lightblue-text");
+  });
+}
+
+function printProjects() 
+{
+  const projects = Object.keys(jsonData.projects);
+  projects.forEach(key => {
+    printListItem(jsonData.projects[key].name, "lightgreen-text");
+  });
+}
+
+function printResearch() 
+{
+  const papers = Object.keys(jsonData.papers);
+  papers.forEach(key => {
+    printListItem(jsonData.papers[key].shortName, "lightgreen-text");
+  });
+}
+
+function isProject(command)
+{
+  const projects = Object.keys(jsonData.projects);
+  return projects.includes(command);
+}
+
+function isPublication(command) 
+{
+  const papers = Object.keys(jsonData.papers)
+  return papers.includes(command);
 }
 
 startBash();
